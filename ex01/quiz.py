570524aef645d27e9ee426b9232e982fbdbd652b
Shintaro <c0a2114285edu.teu.ac.jp>
2022-06-14 16:07:02 +0900

    クイズ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,16 +1,18 @@
 import random
 import datetime
+a=random.dandint(0,2)
 X=["サザエの旦那の名前は?"]
 Y=["カツオの妹の名前は?"]
 Z=["タラオはカツオから見てどんな関係?"]
+XX=["マスオ","ナミヘイ"]
+YY=["ワカメ","コンブ"]
+ZZ=["甥","おいっこ"]
 S=[X,Y,Z]
-def main():
-    seikai=shutudai()
-    kaito=(seikai)
-def shutudai ():
+SS=[XX,YY,ZZ]
+def shutudai (a):
     r=random.choice(S)
     print(r)
-    return r
+    return
     
 def kaito(seikai):
     ans=input("解答を入力してください")
