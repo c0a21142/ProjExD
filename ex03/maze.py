0229877e96eac64dae73b6768bd741713356e389
Shintaro <c0a2114285edu.teu.ac.jp>
2022-06-28 14:36:06 +0900

    練習6

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key=event.keysym
+def key_up(event):
+    global key
+    key=""
+
+
 if __name__=="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん")
@@ -8,7 +16,10 @@ if __name__=="__main__":
     tori=tk.PhotoImage(file="fig/5.png")
     cx,cy=300,400
     canvas.create_image(cx,cy,image=tori,tag="tori")
-    
+    key=""
+    root.bind=("<KeyPress>",key_down)
+    root.bind=("<KeyRelease>",key_up)
+
     root.mainloop()
 
 
