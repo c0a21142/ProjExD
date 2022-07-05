fe82458b6365f90b4313bc1e62066c4efff96bad
Shintaro <c0a2114285edu.teu.ac.jp>
2022-07-05 14:35:27 +0900

    練習4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -11,11 +11,22 @@ def main():
     bg_rect=bgimg_sfc.get_rect()
     sc_sfc.blit(bgimg_sfc, bg_rect)
 
+    kkimg_sfc=pg.image.load("fig/6.png") #Surface
+    kkimg_sfc=pg.transform.rotozoom(kkimg_sfc,0,2.0) #Surface
+    kkimg_rect=kkimg_sfc.get_rect() #Rect
+    kkimg_rect.center=900,400
+
     while True:
         sc_sfc.blit(bgimg_sfc,bg_rect)
+        sc_sfc.blit(kkimg_sfc,kkimg_rect)
         for event in pg.event.get():
             if event.type==pg.QUIT:return
 
+        key_states=pg.key.get_pressed()
+        if key_states[pg.K_UP]==True: kkimg_rect.centery-=1
+        if key_states[pg.K_DOWN]==True: kkimg_rect.centery+=1
+        if key_states[pg.K_LEFT]==True: kkimg_rect.centerx-=1
+        if key_states[pg.K_RIGHT]==True: kkimg_rect.centerx+=1            
         pg.display.update()
         clock.tick(1000)
 
